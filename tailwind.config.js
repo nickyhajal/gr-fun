d916377f78d4e8d3b40cf052306622776cbca7d2
Nicky Hajal <nhajal@gmail.com>
2024-02-20 10:50:57 +0100

    chore: tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./app/views/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/assets/stylesheets/**/*.css",
+    "./app/javascript/**/*.js",
+    "./app/javascript/**/*.ts",
+    "./app/javascript/**/*.tsx",
+    "./app/javascript/**/*.jsx",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        accent: "#37a9af",
+      },
+    },
+  },
+};
