6ced6af1da8c7dc9a9806c48d53ba0c7199b1e83
Nicky Hajal <nhajal@gmail.com>
2024-02-21 12:11:19 +0100

    feat: ProofEvents API endpoint

---
+++ app/controllers/api/v1/proof_events_controller.rb
@@ -0,0 +1,50 @@
+class Api::V1::ProofEventsController < ApplicationController
+  protect_from_forgery with: :null_session
+  before_action :set_proof_event, only: %i[ show edit update destroy ]
+
+  # GET /proof_events or /proof_events.json
+  def index
+    events = ProofEvent.where(product_id: params[:product_id]).order(event_at: :desc).limit(20)
+    render json: events
+  end
+
+  # GET /proof_events/new
+  def new
+    @proof_event = ProofEvent.new
+  end
+
+  # POST /proof_events or /proof_events.json
+  def create
+    @proof_event = ProofEvent.log(proof_event_params)
+    render json: @proof_event
+  end
+
+  def create_demo
+    date = DateTime.now
+    @proof_event = ProofEvent.log({
+      "kind": "message",
+      "event": "purchase",
+      "product_id": 1,
+      "username": "#{Faker::Name.first_name} #{Faker::Name.last_name[0]}.",
+      "location": "#{Faker::Address.city}, #{Faker::Address.state_abbr}",
+      "image": "https://i.pravatar.cc/200?u=",
+      "body": "%user% purchased %product%",
+      "created_at": date, "updated_at": date, "event_at": date,
+    })
+    render json: @proof_event
+  end
+
+  # PATCH/PUT /proof_events/1 or /proof_events/1.json
+  def update
+  end
+
+  # DELETE /proof_events/1 or /proof_events/1.json
+  def destroy
+  end
+
+  private
+
+  def proof_event_params
+    params.require(:proof_event).permit(:type, :message, :image, :username, :product_id, :event, :event_at)
+  end
+end
