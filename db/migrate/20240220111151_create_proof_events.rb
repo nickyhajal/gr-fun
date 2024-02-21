78d14a8893d1df868c5f26aafcb23b59584de737
Nicky Hajal <nhajal@gmail.com>
2024-02-21 12:16:14 +0100

    chore: ProofEvent schema and migrations

---
+++ db/migrate/20240220111151_create_proof_events.rb
@@ -0,0 +1,15 @@
+class CreateProofEvents < ActiveRecord::Migration[7.1]
+  def change
+    create_table :proof_events do |t|
+      t.string :kind
+      t.string :body
+      t.string :image
+      t.string :username
+      t.string :product_id
+      t.string :event
+      t.string :event_at
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,30 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+p "Start data seed..."
+date = 300.minutes.ago
+20.times do |i|
+  id = i + 1
+  ProofEvent.create({
+    "kind": "message",
+    "event": "purchase",
+    "product_id": 1,
+    "username": "#{Faker::Name.first_name} #{Faker::Name.last_name[0]}.",
+    "location": "#{Faker::Address.city}, #{Faker::Address.state_abbr}",
+    "image": "https://i.pravatar.cc/200?u=#{i}",
+    "body": "%user% purchased %product%",
+    "created_at": date, "updated_at": date, "event_at": date,
+  })
+  date = date + rand(4...30).minutes
+end
+
+p "DB Seeded"
