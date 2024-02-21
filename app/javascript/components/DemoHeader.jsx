3ca0997cfcedf1c0f8f29c70633462eb5741acb4
Nicky Hajal <nhajal@gmail.com>
2024-02-21 12:15:19 +0100

    feat: demo header to trigger event

---
+++ app/javascript/components/DemoHeader.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Logo } from "./Logo";
+
+export function DemoHeader() {
+  async function triggerPurchase() {
+    const res = await fetch("/api/v1/proof_events/create_demo", {
+      method: "POST",
+      body: JSON.stringify({ product: "Demo Product" }),
+    }).then((response) => response.json());
+  }
+
+  return (
+    <header className="w-full gap-6 px-8 flex items-center bg-sky-600 font-semibold text-white border-b-2 border-sky-700 pt-4 pb-3">
+      <div>Demo Controls</div>
+      <button
+        class="bg-white/10 hover:bg-white/20 border border-white/20 rounded-md text-sm font-normal px-3 py-1.5"
+        onClick={triggerPurchase}
+      >
+        Trigger Purchase
+      </button>
+    </header>
+  );
+}
--- app/javascript/components/layouts/BaseLayout.tsx
+++ app/javascript/components/layouts/BaseLayout.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { SearchHeader } from "../SearchHeader";
+import { DemoHeader } from "../DemoHeader";
 
 export function BaseLayout({ children }) {
   return (
     <div className="">
+      <DemoHeader />
       <SearchHeader />
       <section className="py-16">
         <div className="">{children}</div>
