2f75b347441bf2334f9de3db691446164c21da78
Nicky Hajal <nhajal@gmail.com>
2024-02-21 12:23:11 +0100

    fix: class -> className in demoheader

--- app/javascript/components/DemoHeader.jsx
+++ app/javascript/components/DemoHeader.jsx
@@ -13,7 +13,7 @@ export function DemoHeader() {
     <header className="w-full gap-6 px-8 flex items-center bg-sky-600 font-semibold text-white border-b-2 border-sky-700 pt-4 pb-3">
       <div>Demo Controls</div>
       <button
-        class="bg-white/10 hover:bg-white/20 border border-white/20 rounded-md text-sm font-normal px-3 py-1.5"
+        className="bg-white/10 hover:bg-white/20 border border-white/20 rounded-md text-sm font-normal px-3 py-1.5"
         onClick={triggerPurchase}
       >
         Trigger Purchase
