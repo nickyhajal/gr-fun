33b92cd505b2c41baf087a7c21a969f5828e1111
Nicky Hajal <nhajal@gmail.com>
2024-02-23 12:09:18 +0100

    feat: allow popup button to take other styles

--- app/javascript/components/ui/PopButton.tsx
+++ app/javascript/components/ui/PopButton.tsx
@@ -1,7 +1,18 @@
 import React from "react";
-export function PopButton({ children, ...props }) {
+import { tw } from "../../util/tw";
+
+interface Props {
+  children?: React.ReactNode;
+  className?: string;
+  href?: string;
+  label: string;
+}
+
+export function PopButton({ children, ...props }: Props) {
   const { label, ...rest } = props;
-  let className = `bg-pink text-black text-[0.97rem] rounded border border-black pt-3 pb-2.5 px-4 ${props.className} transition-all hover:-translate-x-1 hover:-translate-y-1 hover:shadow-[0.25rem_0.25rem_0_#000]`;
+  let className = tw(
+    `bg-pink text-black text-[0.97rem] rounded border border-black pt-3 pb-2.5 px-4 ${props.className} transition-all hover:-translate-x-1 hover:-translate-y-1 hover:shadow-[0.25rem_0.25rem_0_#000] ${props.className}`
+  );
   if (props.href)
     return (
       <a {...rest} className={className} aria-label={label}>
