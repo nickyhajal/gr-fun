a35e7e1c04fd36cf401d347904a96b9de710a490
Nicky Hajal <nhajal@gmail.com>
2024-02-23 03:28:14 +0100

    feat: improved mobile admin support

--- app/javascript/components/layouts/AdminLayout.tsx
+++ app/javascript/components/layouts/AdminLayout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { SearchHeader } from "../SearchHeader";
 import { DemoHeader } from "../DemoHeader";
 
@@ -76,12 +76,28 @@ const pages = [
   ],
 ];
 export function AdminLayout({ children }) {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="h-full">
-      <DemoHeader />
+      <DemoHeader context="admin" />
       <section className="grid grid-cols-[1fr] grid-rows-[1fr] h-full">
-        <nav className="w-[12.8125rem] flex flex-col bg-black row-[span 2] col-[-3] overflow-y-auto">
-          <header className="py-12 px-6">
+        <nav
+          className={`w-full lg:w-[12.8125rem] flex flex-col bg-black lg:row-[span_2] lg:col-[-3] row-[-4] overflow-y-auto ${
+            menuOpen ? "open fixed w-full h-full z-30" : ""
+          }`}
+        >
+          <div className="navbar w-full lg:hidden bg-black p-4 text-white grid items-center gap-3 h-[3.875rem] grid-cols-[auto_1fr_auto]">
+            <a href="https://app.gumroad.com/">
+              <span className="logo-g">&nbsp;</span>
+            </a>
+            <h1 className="text-center text-nowrap w-full">Customer Success</h1>
+            <span
+              onClick={() => setMenuOpen((curr) => !curr)}
+              className="icon toggle text-white"
+              role="button"
+            ></span>
+          </div>
+          <header className="py-12 px-6 hidden lg:block">
             <a href="https://app.gumroad.com/" aria-label="Dashboard">
               <span className="logo-full !w-full text-white leading-[1.2] font-[2.5rem]">
                 &nbsp;
@@ -91,12 +107,14 @@ export function AdminLayout({ children }) {
           {pages.map((s, i) => (
             <section
               key={i}
-              className="flex flex-col mb-12 border-b border-white/50"
+              className={`flex flex-col mb-12 border-b border-white/50 lg:flex
+              ${menuOpen ? "flex" : "hidden"}
+              `}
             >
               {s.map(({ href, label, icon }) => (
                 <a
                   key={label}
-                  href={href}
+                  href="#"
                   className="text-white py-4 px-6 border-t border-white/50 flex items-center gap-4 hover:text-pink"
                 >
                   <div className={`icon icon-${icon} mb-0.5`}></div>
