2ded053e61f4d6ee7a26eca3a08e66a9fc35d32e
Nicky Hajal <nhajal@gmail.com>
2024-02-23 12:09:02 +0100

    tweak: improve input focus styles

--- app/javascript/components/ui/Input.tsx
+++ app/javascript/components/ui/Input.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default (props) => {
+export function Input(props) {
   return (
     <input
       {...props}
-      className={`border border-black rounded-md p-2 ${props.className}`}
+      className={`border border-black rounded-md ring: px-4 py-3 focus:ring-pink focus-visible:outline-none focus-visible:ring-pink focus-visible:ring-[2px] focus-visible:border-black focus:ring-[2px] focus:border-black ${props.className}`}
     />
   );
-};
+}
