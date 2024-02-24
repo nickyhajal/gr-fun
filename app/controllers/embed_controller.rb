ddd1bd6c634f17a34aa9690356f41d21d3fc64e3
Nicky Hajal <nhajal@gmail.com>
2024-02-24 09:29:22 +0100

    fix: embed allow iframe

--- app/controllers/embed_controller.rb
+++ app/controllers/embed_controller.rb
@@ -1,8 +1,15 @@
 class EmbedController < ApplicationController
   skip_before_action :verify_authenticity_token
+  after_action :allow_iframe, only: :embed
 
   def testimonial
     @t = Testimonial.find(params[:id])
     render layout: false
+  end
+
+  private
+
+  def allow_iframe
+    response.headers.except! "X-Frame-Options"
   end
 end
